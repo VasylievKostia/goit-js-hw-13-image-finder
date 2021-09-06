ffa59efe7a69ce51068be0a7033331281c934d7d
VasylievKostia <vasylievk91@gmail.com>
2021-09-06 16:25:50 +0300

    hw13

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
 import './sass/main.scss';
+import galleryCards from './templates/item-template.hbs';
+import { refs } from './refs';
+import PixabyApiService from './apiService';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error, success, defaultModules } from '@pnotify/core';
+
+const pixabyApiService = new PixabyApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+//refs.galleryContainer.addEventListener('click', onClick);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  pixabyApiService.query = e.currentTarget.elements.query.value;
+  pixabyApiService.resetPage();
+  pixabyApiService.fetchArticles().then(data => {
+    errorResult(data);
+    clearArticlesContainer();
+  });
+  refs.loadMoreBtn.classList.add('is-open');
+}
+
+function errorResult(data) {
+  if (data.length === 0) {
+    error({
+      text: 'Изображение не найдено.',
+      delay: 1500,
+    });
+  }
+  pixabyApiService.fetchArticles().then(appArticlesMarkup);
+  return;
+}
+
+function onLoadMore(e) {
+  e.preventDefault();
+
+  pixabyApiService
+    .fetchArticles()
+    .then(appArticlesMarkup)
+    .then(() => {
+      refs.loadMoreBtn.scrollIntoView({ block: 'end', behavior: 'smooth' });
+      success({ text: 'Добавлено больше ответов' });
+    });
+}
+
+function appArticlesMarkup(hits) {
+  refs.galleryContainer.insertAdjacentHTML('beforeend', galleryCards(hits));
+}
+
+function clearArticlesContainer() {
+  refs.galleryContainer.innerHTML = '';
+}
+
+//123123
